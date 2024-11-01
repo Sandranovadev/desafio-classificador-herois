16c86d0d01983b34532b99417c8c44649af97850
SandraOCTecnologia <sandraoctecnologia@gmail.com>
2024-11-01 18:05:01 -0300

    meu primeiro commmit

---
+++ personalizado.js
@@ -0,0 +1,14 @@
+function nome_funcao (){
+    alert("Login ou senha incorreto");
+    }
+
+    function somar(a, b){
+        var total = a + b;
+        alert("Valor da soma:" + total);
+            }
+            
+    function desconto(a, b){
+        var totalDesc = a - b;
+        document.write("Valor final com desconto: " + totalDesc);
+    }
+    desconto(97, 15);
